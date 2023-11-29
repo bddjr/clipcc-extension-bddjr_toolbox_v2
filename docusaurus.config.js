f9c0aa444b88ce330814d81a6315f35b3a740922
半岛的蒟蒻bddjr <109646559+bddjr@users.noreply.github.com>
2023-11-29 11:25:39 +0800

    baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,10 +11,10 @@ const config = {
   favicon: 'img/inset_icon-edit-mr.png',
 
   // Set the production url of your site here
-  url: 'https://btbv2.ccext.bddjr.cn',
+  url: 'https://bddjr.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/clipcc-extension-bddjr_toolbox_v2/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
